7473e72508cf0e2856e688ccedbeaeb2298c1f06
Florian Pallas <mail@pallas-florian.de>
2022-01-19 19:58:36 +0100

    add personal config

---
+++ config.h
@@ -0,0 +1,16 @@
+// Colors
+static const char col_gray1[] = "#212121";
+static const char col_prime[] = "#6200ea";
+
+static const char *colorname[NUMCOLS] = {
+  [INIT] =  col_gray1,
+  [INPUT] = col_prime,
+  [FAILED] = col_gray1,
+};
+
+// Permissions
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+// Misc
+static const int failonclear = 1;
